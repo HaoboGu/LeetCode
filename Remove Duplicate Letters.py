4fc9f073675a5dae7be3b956ed73029006edadaa
JasonGu <ghbo.loveyou@live.com>
2016-01-12 19:39:12 +0800

    Prob.316: Remove Duplicate Letters

---
+++ Remove Duplicate Letters.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        Problem.316
+        """
+        isIn = [0] * 26
+        for i, j in enumerate(s):
+            j.atoi
+            index = int(j)
+            print(index)
+            isIn[index] = 1
+        return isIn
+
+
+s = Solution()
+a = s.removeDuplicateLetters("cbacdcbc")
+print(a)
+
+
+
